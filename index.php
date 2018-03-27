a90438a12028f53e7c08eb82f568ae3a3921f897
Adrien <adrien.dau@gmail.com>
2018-03-27 15:01:34 +0200

    Ajouter la recherche sur Allociné

--- index.php
+++ index.php
@@ -8,18 +8,18 @@ $config = parse_ini_file('config.ini');
 // $application->authorize();
 // $application->openSession();
 
-// Accès distant
 $application = new \alphayax\freebox\utils\Application('com.alphayax.freebox', 'Freebox PHP API', '0.0.1');
 $application->setFreeboxApiHost($config['freebox_api_host']);
 $application->setAppToken($config['freebox_app_token']);
 $application->openSession();
 
-$fileSystemListingService = new \alphayax\freebox\api\v3\services\FileSystem\FileSystemListing($application);
-
 $downloadService = new \alphayax\freebox\api\v3\services\download\Download($application);
-$downloads = $downloadService->getAll();
+$fileSystemListingService = new \alphayax\freebox\api\v3\services\FileSystem\FileSystemListing($application);
+$fileSystemOperationService = new \alphayax\freebox\api\v3\services\FileSystem\FileSystemOperation($application);
 
-foreach ($downloads as $task) {
+$allocine = new AlloHelper;
+
+foreach ($downloadService->getAll() as $task) {
     if (in_array($task->getStatus(), array('done', 'seeding'))) {
         $downloadDir = base64_decode($task->getDownloadDir());
 
@@ -33,7 +33,16 @@ foreach ($downloads as $task) {
                 $movieTitle = str_replace('.', ' ', $matches[1]);
                 $movieYear = $matches[2];
 
-                var_dump($movieTitle, $movieYear);
+                $results = $allocine->search($movieTitle, 1, 10, false, array('movie'))->getArray();
+                if ($results['totalResults']) {
+                    foreach ($results['movie'] as $movie) {
+                        if (strtolower($movie['originalTitle']) == strtolower($movieTitle) &&
+                            $movie['productionYear'] == (int)$movieYear
+                        ) {
+                            // ToDo
+                        }
+                    }
+                }
             }
         } catch (Exception $e) {
             echo $e->getMessage();
