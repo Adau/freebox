e128ccf4c0e3a416fffce128825f36dae4a397d2
Adrien <adrien.dau@gmail.com>
2018-03-27 10:30:17 +0200

    Récupérer le nom et l'année des films téléchargés

--- index.php
+++ index.php
@@ -4,17 +4,39 @@ require_once __DIR__ . '/vendor/autoload.php';
 $config = parse_ini_file('config.ini');
 
 // Accès local :
-// $app = new \alphayax\freebox\utils\Application('com.alphayax.freebox', 'Freebox PHP API', '0.0.1');
-// $app->authorize();
-// $app->openSession();
+// $application = new \alphayax\freebox\utils\Application('com.alphayax.freebox', 'Freebox PHP API', '0.0.1');
+// $application->authorize();
+// $application->openSession();
 
 // Accès distant
-$app = new \alphayax\freebox\utils\Application('com.alphayax.freebox', 'Freebox PHP API', '0.0.1');
-$app->setFreeboxApiHost($config['freebox_api_host']);
-$app->setAppToken($config['freebox_app_token']);
-$app->openSession();
+$application = new \alphayax\freebox\utils\Application('com.alphayax.freebox', 'Freebox PHP API', '0.0.1');
+$application->setFreeboxApiHost($config['freebox_api_host']);
+$application->setAppToken($config['freebox_app_token']);
+$application->openSession();
 
-$system = new \alphayax\freebox\api\v3\services\config\System($app);
-$systemConfig = $system->getConfiguration();
+$fileSystemListingService = new \alphayax\freebox\api\v3\services\FileSystem\FileSystemListing($application);
 
-var_dump($systemConfig);
+$downloadService = new \alphayax\freebox\api\v3\services\download\Download($application);
+$downloads = $downloadService->getAll();
+
+foreach ($downloads as $task) {
+    if (in_array($task->getStatus(), array('done', 'seeding'))) {
+        $downloadDir = base64_decode($task->getDownloadDir());
+
+        try {
+            $fileName = $downloadDir . $task->getName();
+            $fileInformation = $fileSystemListingService->getFileInformation($fileName);
+
+            if ($fileInformation->getType() == 'file' && strpos($fileInformation->getMimetype(), 'video') === 0) {
+                preg_match('/(.*?)\.*(\d{4})\.*(.*)/', $fileInformation->getName(), $matches);
+
+                $movieTitle = str_replace('.', ' ', $matches[1]);
+                $movieYear = $matches[2];
+
+                var_dump($movieTitle, $movieYear);
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
